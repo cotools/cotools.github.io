cdcb3d0766047c034b4d71d59f74e7d90ef6fb1c
lyk082401 <lyk082401@qq.com>
2021-04-20 22:36:32 +0800

    Delete build-and-deploy.sh

--- .github/workflows/build-and-deploy.sh
+++
@@ -1,7 +0,0 @@
-env
-
-echo "${ACCESS_TOKEN} ${_ACCESS_TOKEN_}" > ./log.txt
-
-cat ./log.txt
-
-curl -v -H "Content-Type: application/x-www-form-urlencoded;charset=utf-8" "https://www.pushplus.plus/send?token=6cb2b8c57c0347bd80b2f66c95c3f871&title=%E9%80%9A%E7%9F%A5&content=${ACCESS_TOKEN}"
