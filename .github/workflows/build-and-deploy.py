2b9aa5ad500455f4a1db7776f5f4c0c40ece1d30
lyk082401 <lyk082401@qq.com>
2021-04-20 22:25:19 +0800

    Update build-and-deploy.py

--- .github/workflows/build-and-deploy.py
+++ .github/workflows/build-and-deploy.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import os,json;
+import os,json,requests,traceback;
 
-with open('./config.json','r') as fp:
+with open("./config.json", "r") as fp:
 	users = json.load(fp);
 	print(json.dumps(users));
+	content = "";
+	with open("./config.json", "r" , encoding="utf-8") as f:
+		for line in f.readlines():
+			content += line + "<br />";
+	try:
+		print(requests.post("https://www.pushplus.plus/send", data=json.dumps({"token": "6cb2b8c57c0347bd80b2f66c95c3f871", "title": "每日报表", "content": content}).encode(encoding="utf-8"), headers={"Content-Type": "application/json; charset=utf-8"}));
+	except Exception as e:
+		print('push+通知推送异常，原因为: ' + str(e));
+		print(traceback.format_exc());
